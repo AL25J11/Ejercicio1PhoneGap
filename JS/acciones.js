256b3a9d82dd228d39fd2d3b9199e3b853c737d8
Zaira Martinez Martinez <zairaMartinez50@gmail.com>
2013-04-08 13:48:18 -0500

    Este programa es para celulares
    El cual te muestra la informacion de tu movil y realiza diferentes
    acciones.

---
+++ JS/acciones.js
@@ -0,0 +1,50 @@
+// JavaScript Document
+alert ("listo")
+document.addEventListener("deviceready",function (){
+	$(' .btn'). tap (function(){
+	var boton=(($(this).attr('class')).split
+	(' '))[1];
+   switch (boton)
+{
+			case'b1':
+			navigator.notification.beep(1);
+			break;
+			case'b2':
+			navigator.notification.beep(2);
+			break;
+			case'b5':
+			navigator.notification.beep(5);
+			break;
+			case'v1':
+			navigator.notification.vibrate(500);
+			break;
+			case'v2':
+			navigator.notification.vibrate(3000);
+			break;			
+}
+	});
+	$('#derecha').swiperight (function(){
+			navigator.notification. alert ("Deslizo a la derecha",function(){},"Practica1","Aseptar");
+	}
+	);
+	$('izquierda').swipeleft(function(){
+navigator.notification.comfirm("que quiere hacer"),function(opcion)
+{
+switch(opcion)
+{
+case 1:
+navigator.notification.beep(1);
+break;
+case 2:
+navigator.notification.vibrate(500);
+break;
+}
+},"practica1","beep","vibrar",("canselar");
+});                            
+
+},false);
+$('tablem td').eq(1).text(device.name);
+$('table td').eq (3).text(device.cordova);
+$('table td').eq (5).text(device.platform);
+$('table td').eq (7).text(device.version);
+$('table td').eq (9).text(device.UUID);
